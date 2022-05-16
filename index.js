9ac2c198a8d526f189b827451c7947cbec4a6534
VictorUlian <yctor.ulian@gmail.com>
2022-05-16 15:14:37 -0300

    Feature: Now can Post and GET at endpoint /cart, FIX endpoint LOgin

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ app.post('/login', async (req, res) => {
             const configurationJwt = {expiresIn: 60*60*24*30 } //30dias em segundos
             const token = jwt.sign(sessionId, secretKey, configurationJwt);
             await db.collection("sessions").updateOne({_id: sessao.insertedId}, {$set: {'token': token}})
-            res.send(token);
+            let resposta={token:token, name:user.name}
+            res.send(resposta);
         } else {
             console.log("usuario nao encontrado ou senha incorreta")
             res.sendStatus(404);
@@ -110,7 +111,7 @@ app.get("/usuario", async (req, res) => {
     }
 });
 
-app.delete("/disconnect", async (req,res) => {
+app.delete("/logout", async (req,res) => {
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
     const secretKey = process.env.JWT_SECRET;
@@ -141,13 +142,6 @@ app.delete("/disconnect", async (req,res) => {
 });
 
 app.get("/products", async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer ', '');
-    const secretKey = process.env.JWT_SECRET;
-    if (!token) {//se tiver token
-            console.log("voce nao tem autorizaçao")
-            return res.sendStatus(401);
-    }
     const {type} = req.query;
     const limit = parseInt(req.query.limit);
 	console.log(limit, type)
@@ -243,6 +237,71 @@ app.post("/inserir", async (req, res) => {
     } catch (e) {
         console.error(e);
         res.sendStatus(422);
+    }
+});
+
+app.post("/cart", async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const secretKey = process.env.JWT_SECRET;
+    if (!token) {//se tiver token
+            console.log("voce nao tem autorizaçao")
+            return res.sendStatus(401);
+        }
+    const {idProd, qtd} = req.body;
+    try {
+        const sessionId = jwt.verify(token, secretKey);
+        await mongoClient.connect()
+        const db = mongoClient.db(process.env.DATABASE);
+        const {userId} = await db.collection("sessions").findOne({_id: new ObjectId(sessionId.session)})
+        console.log(userId);
+        if(userId) {
+             // deletando a propriedade password
+            await db.collection("cart").insertOne({
+                idProd: idProd,
+                idUser: userId,
+                qtd:qtd
+            });
+            res.status(201).send("Produto cadastrado no carrinho com sucesso");
+        } else {
+            console.log("Não foi possivel encontrar o usuario nessa sessão")
+            res.sendStatus(401);
+        }
+    } catch (e) {
+        console.error("token invalido" + e);
+        return res.sendStatus(422);
+    }
+});
+
+app.get("/cart", async (req, res) => {
+    const {authorization} = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const secretKey = process.env.JWT_SECRET;
+    if (!token) {//se tiver token
+            console.log("voce nao tem autorizaçao")
+            return res.sendStatus(401);
+        }
+    try {
+        const sessionId = jwt.verify(token, secretKey);
+        await mongoClient.connect()
+        const db = mongoClient.db(process.env.DATABASE);
+        const {userId} = await db.collection("sessions").findOne({_id: new ObjectId(sessionId.session)})
+        console.log(userId);
+        if(userId) {
+            const carrinho = await db.collection("cart").find({idUser: userId}).toArray();
+            //pega o idProd do vetor de objetos carrinho e para cada um deles, encontrar os dados e jogar em um vetor de objeto
+            let resposta = []
+
+
+            console.log(carrinho);
+            res.status(201).send(carrinho);
+        } else {
+            console.log("Não foi possivel encontrar o usuario nessa sessão")
+            res.sendStatus(401);
+        }
+    } catch (e) {
+        console.error("token invalido" + e);
+        return res.sendStatus(422);
     }
 });
 
